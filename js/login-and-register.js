b4b35fdd8072d738713e9a045a31c1beb7334491
Do Huu Duy <duydo0802@gmail.com>
2020-06-24 18:30:22 +0700

    Add file render-filter-type.js, 3 subpages and fix something

--- js/login-and-register.js
+++ js/login-and-register.js
@@ -1,21 +1,21 @@
-let log_in = document.querySelector('.top-navbar__account-log-in');
+let log_in = document.querySelector(".top-navbar__account-log-in");
 console.log(log_in);
-let register = document.querySelector('.top-navbar__account-register');
+let register = document.querySelector(".top-navbar__account-register");
 
-let unaccount = document.querySelector('.top-navbar__account');
+let unaccount = document.querySelector(".top-navbar__account");
 
-let overlay = document.createElement('div');
+let overlay = document.createElement("div");
 overlay.className = "overlay";
 
-let body = document.querySelector('body');
+let body = document.querySelector("body");
 
-let loginForm = document.createElement('div');
-loginForm.className = 'login-form';
+let loginForm = document.createElement("div");
+loginForm.className = "login-form";
 
-log_in.addEventListener('click',(e)=>{
+log_in.addEventListener("click", (e) => {
   e.preventDefault();
   console.log(e.target);
-  if(e.target == log_in) {
+  if (e.target == log_in) {
     body.appendChild(overlay);
     loginForm.innerHTML = `
     <form>
@@ -29,41 +29,51 @@ log_in.addEventListener('click',(e)=>{
     </div>
     <p class = "invalid-account"></p>
     <button type="submit" class="btn btn-primary btn-login first">Đăng nhập</button>
-    </form>`
+    </form>`;
     overlay.appendChild(loginForm);
-    overlay.addEventListener('click',(e)=>{
+    overlay.addEventListener("click", (e) => {
       e.preventDefault();
-      if(e.target == overlay) overlay.remove();
-      let buttonSubmit1 = document.querySelector('.btn-login.first');
-      if(e.target == buttonSubmit1){
-        let emailLogin = document.getElementById('email-login').value;
-        let passwordLogin = document.getElementById('password-login').value;
-        let p = document.querySelector('.invalid-account');
+      if (e.target == overlay) overlay.remove();
+      let buttonSubmit1 = document.querySelector(".btn-login.first");
+      if (e.target == buttonSubmit1) {
+        let emailLogin = document.getElementById("email-login").value;
+        let passwordLogin = document.getElementById("password-login").value;
+        let p = document.querySelector(".invalid-account");
         let status = 0;
-        for(let i = 0; i < user.length; i++){
-          if(user[i].email == emailLogin && user[i].password == passwordLogin){
+        for (let i = 0; i < user.length; i++) {
+          if (
+            user[i].email == emailLogin &&
+            user[i].password == passwordLogin
+          ) {
             status = 1;
             overlay.remove();
             unaccount.removeChild(log_in);
             unaccount.removeChild(register);
-            let accountLogo = document.createElement('div');
-            accountLogo.className = 'account-logo';
-            let accountName = document.createElement('div');
-            accountName.className = 'account-name';
+            let accountLogo = document.createElement("div");
+            accountLogo.className = "account-logo";
+            let accountName = document.createElement("div");
+            accountName.className = "account-name";
             accountName.innerHTML = `${user[i].username}`;
+            let accountLogout = document.createElement("div");
+            accountLogout.className = "account-logout";
+            accountLogout.innerHTML = "Thoát";
             unaccount.appendChild(accountName);
             unaccount.appendChild(accountLogo);
+            unaccount.appendChild(accountLogout);
           }
         }
-        if(status == 0) {
-          p.innerHTML = 'Tài khoản và mật khẩu không chính xác. Mời bạn nhập lại';
+        if (status == 0) {
+          p.innerHTML =
+            "Tài khoản và mật khẩu không chính xác. Mời bạn nhập lại";
         }
       }
-    })
+    });
   }
-})
+});
 
-register.addEventListener('click',(e)=>{
+console.log(status);
+
+register.addEventListener("click", (e) => {
   e.preventDefault();
   body.appendChild(overlay);
   loginForm.innerHTML = `
@@ -89,43 +99,44 @@ register.addEventListener('click',(e)=>{
   </div>
   <p class = "invalid-account"></p>
   <button type="submit" class="btn btn-primary btn-login second">Đăng kí</button>
-  </form>`
+  </form>`;
   overlay.appendChild(loginForm);
-  overlay.addEventListener('click',(e)=>{
+  overlay.addEventListener("click", (e) => {
     e.preventDefault();
-    if(e.target == overlay) overlay.remove();
-    let buttonSubmit2 = document.querySelector('.btn-login.second');
-    let p = document.querySelector('.invalid-account');
-    if(e.target == buttonSubmit2) {
-      let emailRegisterRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/igm;
-      let emailRegister = document.getElementById('email-register').value;
+    if (e.target == overlay) overlay.remove();
+    let buttonSubmit2 = document.querySelector(".btn-login.second");
+    let p = document.querySelector(".invalid-account");
+    if (e.target == buttonSubmit2) {
+      let emailRegisterRegex = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim;
+      let emailRegister = document.getElementById("email-register").value;
       let passwordRegex = /\w{6,32}/;
-      let username = document.getElementById('username-register').value; 
-      let passwordRegister = document.getElementById('password-register').value;
-      let confirmPassword = document.getElementById('confirm-password-register').value;
-      let account = new User(
-        username,
-        emailRegister,
-        passwordRegister
-      );
-      console.log(checkAccount(user,account));
-      if(emailRegisterRegex.test(emailRegister) && passwordRegex.test(passwordRegister) 
-        && passwordRegex.test(username) && passwordRegister == confirmPassword 
-        && checkAccount(user,account)){
+      let username = document.getElementById("username-register").value;
+      let passwordRegister = document.getElementById("password-register").value;
+      let confirmPassword = document.getElementById("confirm-password-register")
+        .value;
+      let account = new User(username, emailRegister, passwordRegister);
+      console.log(checkAccount(user, account));
+      if (
+        emailRegisterRegex.test(emailRegister) &&
+        passwordRegex.test(passwordRegister) &&
+        passwordRegex.test(username) &&
+        passwordRegister == confirmPassword &&
+        checkAccount(user, account)
+      ) {
         alert("Đăng kí thành công !!");
         user.push(account);
         console.log(user);
         overlay.remove();
       } else {
-        p.innerHTML = 'Tài khoản không hợp lệ hoặc địa chỉ email đã tồn tại';
+        p.innerHTML = "Tài khoản không hợp lệ hoặc địa chỉ email đã tồn tại";
       }
     }
-  })
-})
+  });
+});
 
-function checkAccount(arr,obj){
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i].email == obj.email){
+function checkAccount(arr, obj) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].email == obj.email) {
       return false;
     }
   }
